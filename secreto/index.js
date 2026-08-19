391855a9b5ded333976bb2b6f7162ecf7321f16f
jmendoza93 <jeanmendoza945@gmail.com>
2026-08-18 20:43:05 -0500

    Fix app

--- secreto/index.js
+++ secreto/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
-const app  = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
+
+const miVariable = process.env.valor;
+
 app.get('/secreto', (req, res) => {
-    res.json({ message: 'Secret: $valor' });
-})
+  res.json({
+    message: `Secret: ${miVariable}`
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`🚀 Servidor en puerto ${port}`);
 });
